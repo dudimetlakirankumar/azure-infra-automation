becdec7cd1bbc611a00659e1e4a279024bb1e4ee
dudimetlakirankumar <107704110+dudimetlakirankumar@users.noreply.github.com>
2024-02-20 17:11:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,23 +11,58 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "my-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "my-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                      = "my-nic"
+  location                  = azurerm_resource_group.example.location
+  resource_group_name       = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "my-ip-config"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "example" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = var.location
-  size                = var.vm_size
+  name                  = "my-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  size                  = "Standard_DS1_v2"
 
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
-  }
+  admin_username = "adminuser"
+  admin_password = "P@ssw0rd123"
 
   os_disk {
     caching              = "ReadWrite"
@@ -43,4 +78,3 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   computer_name  = "hostname"
 }
-
